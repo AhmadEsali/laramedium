ff32a0bb0ba30d5681b9a0eac28c5c912b1a7cab
ahmad esali <ahmad_esali@hotmail.com>
2020-06-26 23:28:08 +0300

    make the active class in dashboard links dynamic

--- resources/views/dashboard/includes/sidebar.blade.php
+++ resources/views/dashboard/includes/sidebar.blade.php
@@ -1,79 +1,79 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
-       <!-- Brand Logo -->
-       <a href="index3.html" class="brand-link">
-         <img src="{{asset('assets/admin')}}/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
-              style="opacity: .8">
-         <span class="brand-text font-weight-light">Lara Medium</span>
-       </a>
-   
-       <!-- Sidebar -->
-       <div class="sidebar">
-         <!-- Sidebar user panel (optional) -->
-         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-           <div class="image">
-             <img src="{{asset('assets/admin')}}/dist/img/avatar.png" class="img-circle elevation-2" alt="User Image">
-           </div>
-           <div class="info">
-             <a href="#" class="d-block"> 
-              @if (Auth::user())
-                  
-              {{Auth::user()->name }}</a>
-              @endif 
-           </div>
-         </div>
-   
-         <!-- Sidebar Menu -->
-         <nav class="mt-2">
-           <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-             <!-- Add icons to the links using the .nav-icon class
-                  with font-awesome or any other icon font library -->
-             <li class="nav-item ">
+  <!-- Brand Logo -->
+  <a href="index3.html" class="brand-link">
+    <img src="{{asset('assets/admin')}}/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+         style="opacity: .8">
+    <span class="brand-text font-weight-light">Lara Medium</span>
+  </a>
 
-             <a href="{{route('dashboard.index')}}" class="nav-link active">
-                 <i class="nav-icon fas fa-tachometer-alt"></i>
-                 <p>
-                   Dashboard
-                 </p>
-               </a>
-           
-             </li>
-             <li class="nav-item">
-               <a href="{{route('post.index')}}" class="nav-link">
-                 <i class="nav-icon fas fa-th"></i>
-                 <p>
-                   Posts
-                 </p>
-               </a>
-             </li>
-             <li class="nav-item">
-               <a href="{{route('tag.index')}}" class="nav-link">
-                 <i class="nav-icon fas fa-tag"></i>
-                 <p>
-                   Tags
-                 </p>
-               </a>
-             </li>
-          
-             <li class="nav-item">
-              <a class="nav-link" href="{{ route('logout') }}"
+  <!-- Sidebar -->
+  <div class="sidebar">
+    <!-- Sidebar user panel (optional) -->
+    <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+      <div class="image">
+        <img src="{{asset('assets/admin')}}/dist/img/avatar.png" class="img-circle elevation-2" alt="User Image">
+      </div>
+      <div class="info">
+        <a href="#" class="d-block"> 
+         @if (Auth::user())
+             
+         {{Auth::user()->name }}</a>
+         @endif 
+      </div>
+    </div>
 
-              onclick="event.preventDefault();
-                            document.getElementById('logout-form').submit();">
-              <i class="nav-icon fas fa-sign-out-alt"></i>
+    <!-- Sidebar Menu -->
+    <nav class="mt-2">
+      <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+        <!-- Add icons to the links using the .nav-icon class
+             with font-awesome or any other icon font library -->
+        <li class="nav-item ">
 
-                <p>
+        <a href="{{route('dashboard.index')}}" class="nav-link  {{ request()->is('dashboard') ? 'active' : '' }}">
+            <i class="nav-icon fas fa-tachometer-alt"></i>
+            <p>
+              Dashboard
+            </p>
+          </a>
+      
+        </li>
+        <li class="nav-item">
+          <a href="{{route('post.index')}}" class="nav-link  {{ request()->is('dashboard/post*') ? 'active' : '' }}">
+            <i class="nav-icon fas fa-th"></i>
+            <p>
+              Posts
+            </p>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="{{route('tag.index')}}" class="nav-link  {{ request()->is('dashboard/tag*') ? 'active' : '' }}">
+            <i class="nav-icon fas fa-tag"></i>
+            <p>
+              Tags
+            </p>
+          </a>
+        </li>
+     
+        <li class="nav-item">
+         <a class="nav-link" href="{{ route('logout') }}"
 
-                  {{ __('Logout') }}
-                </p>
-           </a>
-            </li>
-            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-              @csrf
-          </form>
-            
-           </ul>
-         </nav>
-         <!-- /.sidebar-menu -->
-       </div>
-       <!-- /.sidebar -->
-     </aside>
+         onclick="event.preventDefault();
+                       document.getElementById('logout-form').submit();">
+         <i class="nav-icon fas fa-sign-out-alt"></i>
+
+           <p>
+
+             {{ __('Logout') }}
+           </p>
+      </a>
+       </li>
+       <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+         @csrf
+     </form>
+       
+      </ul>
+    </nav>
+    <!-- /.sidebar-menu -->
+  </div>
+  <!-- /.sidebar -->
+</aside>
